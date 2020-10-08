5474c38069bb03754a03b058030562bd7031b421
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2020-10-08 00:00:17 -0300

    include console app for consume api and download pdf file

---
+++ SendFiles/WebApiClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var file = await DownloadFile();
+
+            File.WriteAllBytes(@"C:\Temp\Example.pdf", file);
+        }
+
+        public static async Task<byte[]> DownloadFile()
+        {
+            using (var client = new HttpClient())
+            {
+                using (var result = await client.GetAsync("http://localhost:5001/report/v1"))
+                {
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return await result.Content.ReadAsByteArrayAsync();
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
